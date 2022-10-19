93df2a1bf536f88b2b94911fcd7868c540474164
Edhunury <nagalakshmi.edhunury@accenture.com>
2022-10-19 16:00:50 +0530

    first commit

---
+++ DownloadFileUrl/DownloadFileUrl/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DownloadFileUrl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(26, 48);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(476, 26);
+            this.txtFile.TabIndex = 0;
+            // 
+            // btnFile
+            // 
+            this.btnFile.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnFile.Location = new System.Drawing.Point(562, 51);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(158, 49);
+            this.btnFile.TabIndex = 1;
+            this.btnFile.Text = "Select File";
+            this.btnFile.UseVisualStyleBackColor = false;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.txtFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnFile;
+    }
+}
+
---
+++ DownloadFileUrl/DownloadFileUrl/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownloadFileUrl
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog open = new OpenFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            open.Filter = "Text Files(*.png)|*.png";     
+            if(open.ShowDialog() == DialogResult.OK)
+            {
+                txtFile.Text=Path.GetFileName(open.FileName);
+                uploadFile(open.FileName);
+            }
+        }
+
+        private void uploadFile(string fileName)
+        {
+            try
+            {
+                var client = new WebClient();
+
+                var uri = new Uri("http://localhost:8080/download/index.php/");
+                {
+                    client.Headers.Add("fileName", System.IO.Path.GetFileName(fileName));
+                    client.UploadFileAsync(uri, fileName);
+                    MessageBox.Show("Upload file successfully!");
+
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
